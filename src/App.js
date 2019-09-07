2ef2be940444acc8c4d8a9531bf49309ab09c784
Raphael Scheible <mail@raphiniert.com>
2019-09-07 18:55:35 +0200

    rest login

--- src/App.js
+++ src/App.js
@@ -1,34 +1,52 @@
-// in App.js
-import React, { Component } from 'react';
-import buildGraphQLProvider from 'ra-data-graphql-simple';
-import { Admin } from 'react-admin';
+import React from 'react';
+import { Admin, Resource } from 'react-admin';
+import postgrestClient from 'aor-postgrest-client';
+import { List, Datagrid, TextField, NumberField } from 'react-admin';
+
+import { ShowButton, EditButton, Edit, SimpleForm, DisabledInput, TextInput, NumberInput } from 'react-admin';
+import { Create} from 'react-admin';
+import { Show, SimpleShowLayout } from 'react-admin';
 import authProvider from "./authProvider";
 
-// import { PostCreate, PostEdit, PostList } from './posts';
-
-class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { dataProvider: null };
-    }
-    componentDidMount() {
-        buildGraphQLProvider({ clientOptions: { uri: 'http://localhost:8080/graphql/simple' }})
-            .then(dataProvider => this.setState({ dataProvider }));
-    }
-
-    render() {
-        const { dataProvider } = this.state;
-
-        if (!dataProvider) {
-            return <div>Loading</div>;
-        }
-
-        return (
-            <Admin dataProvider={dataProvider} authProvider={authProvider}>
-                {/*<Resource name="Post" list={PostList} edit={PostEdit} create={PostCreate} remove={Delete} />*/}
-            </Admin>
-        );
-    }
-}
+const TodoList = (props) => (
+    <List {...props}>
+        <Datagrid>
+            <ShowButton />
+            <EditButton />
+            <TextField source="author" />
+            <NumberField source="count" />
+        </Datagrid>
+    </List>
+);
+export const TodoShow = (props) => (
+    <Show {...props}>
+        <SimpleShowLayout>
+            <TextField source="author" />
+            <NumberField source="count" />
+        </SimpleShowLayout>
+    </Show>
+);
+export const TodoEdit = (props) => (
+    <Edit {...props}>
+        <SimpleForm>
+            <DisabledInput source="id" />
+            <TextInput source="author" />
+            <NumberInput source="count" />
+        </SimpleForm>
+    </Edit>
+);
+export const TodoCreate = (props) => (
+    <Create {...props}>
+        <SimpleForm>
+            <TextInput source="author" />
+            <NumberInput source="count" />
+        </SimpleForm>
+    </Create>
+);
+const App = () => (
+    <Admin authProvider={authProvider} dataProvider={postgrestClient('http://localhost:3000')}>
+        <Resource name="todos" show={TodoShow} create={TodoCreate} edit={TodoEdit} list={TodoList} />
+    </Admin>
+);
 
 export default App;
--- src/authProvider.js
+++ src/authProvider.js
@@ -1,32 +1,43 @@
-import { AUTH_LOGIN, AUTH_LOGOUT, AUTH_ERROR, AUTH_CHECK } from 'react-admin';
+import { AUTH_LOGIN, AUTH_LOGOUT, AUTH_ERROR, AUTH_GET_PERMISSIONS, AUTH_CHECK } from 'react-admin';
+import decodeJwt from 'jwt-decode';
 
 export default (type, params) => {
-    // called when the user attempts to log in
     if (type === AUTH_LOGIN) {
-        const { username } = params;
-        localStorage.setItem('username', username);
-        // accept all username/password combinations
-        return Promise.resolve();
+        const { username, password } = params;
+        const request = new Request('http://localhost:3000/rpc/login', {
+            method: 'POST',
+            body: JSON.stringify({ email: username, password }),
+            headers: new Headers({ 'Content-Type': 'application/json' }),
+        });
+        return fetch(request)
+            .then(response => {
+                if (response.status < 200 || response.status >= 300) {
+                    throw new Error(response.message);
+                }
+                return response.json();
+            })
+            .then((data) => {
+                let token = data[0].token;
+                const decodedToken = decodeJwt(token);
+                console.log(decodedToken);
+                localStorage.setItem('token', token);
+                localStorage.setItem('role', decodedToken.role);
+            });
     }
-    // called when the user clicks on the logout button
     if (type === AUTH_LOGOUT) {
-        localStorage.removeItem('username');
+        localStorage.removeItem('token');
+        localStorage.removeItem('role');
         return Promise.resolve();
     }
-    // called when the API returns an error
     if (type === AUTH_ERROR) {
-        const { status } = params;
-        if (status === 401 || status === 403) {
-            localStorage.removeItem('username');
-            return Promise.reject();
-        }
-        return Promise.resolve();
+        // ...
     }
-    // called when the user navigates to a new location
     if (type === AUTH_CHECK) {
-        return localStorage.getItem('username')
-            ? Promise.resolve()
-            : Promise.reject();
+        return localStorage.getItem('token') ? Promise.resolve() : Promise.reject();
+    }
+    if (type === AUTH_GET_PERMISSIONS) {
+        const role = localStorage.getItem('role');
+        return role ? Promise.resolve(role) : Promise.reject();
     }
     return Promise.reject('Unknown method');
 };
