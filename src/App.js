9d21fad2c3007d37194782b6113541c605398140
Raphael Scheible <mail@raphiniert.com>
2019-09-10 15:35:17 +0200

    login working

--- src/App.js
+++ src/App.js
@@ -1,50 +1,23 @@
 import React from 'react';
-import { Admin, Resource } from 'react-admin';
+import { fetchUtils, Admin, Resource } from 'react-admin';
 import postgrestClient from 'aor-postgrest-client';
-import { List, Datagrid, TextField, NumberField } from 'react-admin';
 
-import { ShowButton, EditButton, Edit, SimpleForm, DisabledInput, TextInput, NumberInput } from 'react-admin';
-import { Create} from 'react-admin';
-import { Show, SimpleShowLayout } from 'react-admin';
+import {TodoShow, TodoList, TodoEdit, TodoCreate} from './Todo';
 import authProvider from "./authProvider";
 
-const TodoList = (props) => (
-    <List {...props}>
-        <Datagrid>
-            <ShowButton />
-            <EditButton />
-            <TextField source="author" />
-            <NumberField source="count" />
-        </Datagrid>
-    </List>
-);
-export const TodoShow = (props) => (
-    <Show {...props}>
-        <SimpleShowLayout>
-            <TextField source="author" />
-            <NumberField source="count" />
-        </SimpleShowLayout>
-    </Show>
-);
-export const TodoEdit = (props) => (
-    <Edit {...props}>
-        <SimpleForm>
-            <DisabledInput source="id" />
-            <TextInput source="author" />
-            <NumberInput source="count" />
-        </SimpleForm>
-    </Edit>
-);
-export const TodoCreate = (props) => (
-    <Create {...props}>
-        <SimpleForm>
-            <TextInput source="author" />
-            <NumberInput source="count" />
-        </SimpleForm>
-    </Create>
-);
+// sending httpOnly cookie for login
+const httpClient = (url, options = {}) => {
+    options = {
+        credentials: 'include'
+    }
+
+    return fetchUtils.fetchJson(url, options);
+}
+
+const dataProvider = postgrestClient('rest', httpClient);
+
 const App = () => (
-    <Admin authProvider={authProvider} dataProvider={postgrestClient('http://localhost:3000')}>
+    <Admin authProvider={authProvider} dataProvider={dataProvider}>
         <Resource name="todos" show={TodoShow} create={TodoCreate} edit={TodoEdit} list={TodoList} />
     </Admin>
 );
--- src/authProvider.js
+++ src/authProvider.js
@@ -1,10 +1,9 @@
 import { AUTH_LOGIN, AUTH_LOGOUT, AUTH_ERROR, AUTH_GET_PERMISSIONS, AUTH_CHECK } from 'react-admin';
-import decodeJwt from 'jwt-decode';
 
 export default (type, params) => {
     if (type === AUTH_LOGIN) {
         const { username, password } = params;
-        const request = new Request('http://localhost:3000/rpc/login', {
+        const request = new Request('rest/rpc/login?select=me', {
             method: 'POST',
             body: JSON.stringify({ email: username, password }),
             headers: new Headers({ 'Content-Type': 'application/json' }),
@@ -17,26 +16,33 @@ export default (type, params) => {
                 return response.json();
             })
             .then((data) => {
-                let token = data[0].token;
-                const decodedToken = decodeJwt(token);
-                console.log(decodedToken);
-                localStorage.setItem('token', token);
-                localStorage.setItem('role', decodedToken.role);
+                localStorage.setItem('me', JSON.stringify(data.me));
             });
     }
     if (type === AUTH_LOGOUT) {
-        localStorage.removeItem('token');
-        localStorage.removeItem('role');
-        return Promise.resolve();
+        const request = new Request('rest/rpc/logout', {
+            method: 'POST',
+            headers: new Headers({ 'Content-Type': 'application/json' }),
+        });
+        return fetch(request)
+            .then(response => {
+                if (response.status < 200 || response.status >= 300) {
+                    throw new Error(response.message);
+                }
+                return response.json();
+            })
+            .then((data) => {
+                localStorage.removeItem('me');
+            });
     }
     if (type === AUTH_ERROR) {
         // ...
     }
     if (type === AUTH_CHECK) {
-        return localStorage.getItem('token') ? Promise.resolve() : Promise.reject();
+        return localStorage.getItem('me') ? Promise.resolve() : Promise.reject();
     }
     if (type === AUTH_GET_PERMISSIONS) {
-        const role = localStorage.getItem('role');
+        const role = JSON.parse(localStorage.getItem('me')).role;
         return role ? Promise.resolve(role) : Promise.reject();
     }
     return Promise.reject('Unknown method');
